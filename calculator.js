7d66d688cd43a65effc04c638049184907192bef
Abdelrahman Elsafty <abdelrhmanelsafty74@gmail.com>
2025-12-17 04:31:50 +0200

    logic script

--- calculator.js
+++ calculator.js
@@ -1,8 +1,15 @@
-const add = (n1, n2) => n1 + n2;
-const subtract = (n1, n2) => n1 - n2;
-const multiply = (n1, n2) => n1 * n2;
-const divide = (n1, n2) => n1 / n2;
-
+function add(n1,n2){
+    return n1 + n2;
+}
+function subtract(n1,n2){
+    return n1 - n2;
+}   
+function multiply(n1,n2){
+    return n1 * n2;
+}
+function divide(n1,n2){
+    return n1 / n2;
+}
 document.getElementById('add').addEventListener('click', function() {
     const n1 = parseFloat(document.getElementById('number1').value) || 0;
     const n2 = parseFloat(document.getElementById('number2').value) || 0;
